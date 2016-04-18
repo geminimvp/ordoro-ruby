d00e969f090711faa6bef1c3634c7894716ad9fe
Jim Kane <fastjames@gmail.com>
2016-04-17 20:46:24 -0500

    Account for Address under Supplier
    Most supplier attributes appear to be included in an address when
    reading from Ordoro. Since we're not writing Suppliers yet, we can use
    that single attribute.

--- lib/ordoro/record/supplier.rb
+++ lib/ordoro/record/supplier.rb
@@ -4,19 +4,7 @@ module Ordoro
   module Record
     class Supplier < Base
 
-      attribute :name, String
-      attribute :company, String
-      attribute :street1, String
-      attribute :street2, String
-      attribute :city, String
-      attribute :state, String
-      attribute :zip, String
-      attribute :country, String
-      attribute :reference_number, String
-      attribute :email, String
-      attribute :phone, String
-      attribute :fax, String
-      attribute :cart_address_id, String
+      attribute :address, Ordoro::Record::Address
 
     end
   end
